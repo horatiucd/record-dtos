3615ea0ca20d1ab004b670b494fe77329ace2758
horatiu.dan <horatiu.dan@tangoe.com>
2025-04-17 16:42:48 +0300

    Enhances DTOs.

--- src/main/java/com/hcd/recorddtos/service/dto/Forecast.java
+++ src/main/java/com/hcd/recorddtos/service/dto/Forecast.java
@@ -1,13 +1,17 @@
 package com.hcd.recorddtos.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record Forecast(Props properties) {
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record Props(List<Period> periods) {}
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record Period(String name,
                          String startTime, String endTime,
                          int temperature, @JsonProperty("temperatureUnit") String unit,
--- src/main/java/com/hcd/recorddtos/service/dto/PointMetadata.java
+++ src/main/java/com/hcd/recorddtos/service/dto/PointMetadata.java
@@ -1,7 +1,11 @@
 package com.hcd.recorddtos.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PointMetadata(Properties properties) {
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record Properties(String forecast) {}
 }
 
